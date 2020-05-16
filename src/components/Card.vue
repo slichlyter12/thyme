d673ca758bea435f2ebe901a30e0da7c2fefd33d
Sam Lichlyter <slichlyter12@me.com>
2020-05-16 16:33:59 -0700

    recipe cards are showing up and background added

---
+++ src/components/Card.vue
@@ -0,0 +1,38 @@
+<template>
+    <v-card
+        class="mx-3 d-block"
+        max-width="344"
+        hover
+    >
+        <v-list-item three-line>
+            <v-list-item-content>
+                <div class="overline mb-4">{{ duration }}</div>
+                <v-list-item-title class="headline mb-1">{{ title }}</v-list-item-title>
+                <v-list-item-subtitle>
+                    <ul>
+                        <li v-for="ingredient in ingredients" :key="ingredient">{{ ingredient }}</li>
+                    </ul>
+                </v-list-item-subtitle>
+                <hr />
+                <v-list-item-subtitle>
+                    <ol>
+                        <li v-for="step in steps" :key="step">{{ step }}</li>
+                    </ol>
+                </v-list-item-subtitle>
+            </v-list-item-content>
+        </v-list-item>
+    </v-card>
+</template>
+<script>
+export default {
+    name: 'Card',
+
+    props: {
+        id: Number,
+        duration: String,
+        title: String,
+        ingredients: Array,
+        steps: Array,
+    }
+}
+</script>
